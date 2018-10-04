1c5c92bff02a676a674d8c888cd3c7e5ff877b97
Kale-ab <kale-ab.tessera@entelect.co.za>
2018-10-04 11:24:53 +0200

    Re-worked whole website and added new components

--- src/App.js
+++ src/App.js
@@ -1,59 +1,79 @@
-import React, { Component } from 'react';
-import ReactGA from 'react-ga';
-import $ from 'jquery';
-import './App.css';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import About from './Components/About';
-import Resume from './Components/Resume';
-import Contact from './Components/Contact';
-import Testimonials from './Components/Testimonials';
-import Portfolio from './Components/Portfolio';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Redirect } from "react-router";
+
+// Featured
+import Index from "./views/Index";
+import About from "./views/About";
+import Projects from "./views/Projects";
+import Resume from "./views/Resume";
+import Stats from "./views/Stats";
+import Contact from "./views/Contact";
+
+import NotFound from "./views/NotFound";
+
+// All of our CSS
+import "./css/App.scss";
+// url search parameters, in our case ?path=[:slug]
+const params = window.location.search;
+
+let path = "/"; // path we will redirect to if param exits
+
+if (params.search("path=") > -1) {
+  // check if exists
+  path = params.substr(1 + "path=".length).trim();
+}
 
 class App extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     foo: "bar",
+  //     resumeData: {}
+  //   };
 
-  constructor(props){
-    super(props);
-    this.state = {
-      foo: 'bar',
-      resumeData: {}
-    };
+  //   ReactGA.initialize("UA-110570651-1");
+  //   ReactGA.pageview(window.location.pathname);
+  // }
 
-    ReactGA.initialize('UA-110570651-1');
-    ReactGA.pageview(window.location.pathname);
+  // getResumeData() {
+  //   $.ajax({
+  //     url: "/resumeData.json",
+  //     dataType: "json",
+  //     cache: false,
+  //     success: function(data) {
+  //       this.setState({ resumeData: data });
+  //     }.bind(this),
+  //     error: function(xhr, status, err) {
+  //       console.log(err);
+  //       alert(err);
+  //     }
+  //   });
+  // }
 
-  }
-
-  getResumeData(){
-    $.ajax({
-      url:'/resumeData.json',
-      dataType:'json',
-      cache: false,
-      success: function(data){
-        this.setState({resumeData: data});
-      }.bind(this),
-      error: function(xhr, status, err){
-        console.log(err);
-        alert(err);
-      }
-    });
-  }
-
-  componentDidMount(){
-    this.getResumeData();
-  }
+  // componentDidMount() {
+  //   this.getResumeData();
+  // }
 
   render() {
     return (
-      <div className="App">
-        <Header data={this.state.resumeData.main}/>
-        <About data={this.state.resumeData.main}/>
-        <Resume data={this.state.resumeData.resume}/>
-        <Portfolio data={this.state.resumeData.portfolio}/>
-        <Testimonials data={this.state.resumeData.testimonials}/>
-        <Contact data={this.state.resumeData.main}/>
-        <Footer data={this.state.resumeData.main}/>
-      </div>
+      <Router>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (path !== "/" ? <Redirect to={path} /> : <Index />)}
+          />
+          <Route path="/about" component={About} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/stats" component={Stats} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/resume" component={Resume} />
+          {/* Only useful in development mode */}
+          <Route component={NotFound} status={404} />
+        </Switch>
+      </Router>
     );
   }
 }
